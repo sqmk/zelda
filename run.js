0474acece0a1756b1bc82b5a856b0daee58201d7
Michael K. Squires <sqmk@php.net>
2016-09-06 15:55:55 -0500

    Fix current time

--- run.js
+++ run.js
@@ -6,41 +6,41 @@ let say    = require('say');
 
 
 let client = new huejay.Client({
-		host: "10.0.1.2",
-		username: "06aEKvJIr3o-9m3yLE-5Jm2S9K0nY8a1MpiA9afx"
+  host: "10.0.1.2",
+  username: "06aEKvJIr3o-9m3yLE-5Jm2S9K0nY8a1MpiA9afx"
 });
 
 var busy    = false;
 let lastChanged = 0;
 
-let currentTime = moment().format('h:mm a');
-
 function checkMotion() {
-		if (busy === true) {
-				return;
-		}
+  if (busy === true) {
+    return;
+  }
 
-		busy = true;
+  busy = true;
 
-		client.sensors.getById(35)
-				.then(sensor => {
-						let changed    = moment(`${sensor.state.lastUpdated}Z`);
-						let now        = moment();
-						let difference = now.diff(changed, 'seconds');
+  client.sensors.getById(35)
+    .then(sensor => {
+      let changed    = moment(`${sensor.state.lastUpdated}Z`);
+      let now        = moment();
+      let difference = now.diff(changed, 'seconds');
 
-						if (sensor.state.status == 1 && difference > 0 && lastChanged != changed.format()) {
-								lastChanged = changed.format();
+      if (sensor.state.status == 1 && difference > 0 && lastChanged != changed.format()) {
+        lastChanged = changed.format();
 
-								return say.speak(`Welcome to the lab. The time is ${currentTime}.. Your access has been logged.`, "Samantha", 1.0, () => {
-										busy = false;
-								});
-						}
+        let currentTime = moment().format('h:mm a');
 
-						busy = false;
-				})
-				.catch(err => {
-						busy = false;
-				});
+        return say.speak(`Welcome to the lab. The time is ${currentTime}.. Your access has been logged.`, "Samantha", 1.0, () => {
+          busy = false;
+        });
+      }
+
+      busy = false;
+    })
+    .catch(err => {
+      busy = false;
+    });
 }
 
 setInterval(checkMotion, 1000);
